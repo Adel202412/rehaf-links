60529f8855529f46bf7f463f8dc4170ecaa1e4c0
Adel202412 <adel.alsoufi.90@gmail.com>
2025-09-06 12:18:45 +0400

    Create rehaf-footer.js

---
+++ rehaf-automation/rehaf-footer.js
@@ -0,0 +1,12 @@
+// rehaf-footer.js
+export function initRehafFooter(container) {
+  if (!container) return;
+  container.innerHTML = `
+    <section class="arabic">
+      <div class="container">
+        <div class="headline">كن الأوّل في السوق</div>
+        <div class="sub">مع REHAF، المستقبل يبدأ الآن.</div>
+      </div>
+    </section>
+  `;
+}
